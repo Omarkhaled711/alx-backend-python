de7e481d52ced77cee71b2fe594767285cddedda
Omar Khaled <o.khaled6789@gmail.com>
2024-01-12 05:01:58 +0200

    edited function name in 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ module for the eighth task
 from typing import Tuple, Union
 
 
-def sum_mixed_list(k: str,  v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str,  v: Union[int, float]) -> Tuple[str, float]:
     """
     It takes a string k and an int OR float v as arguments and returns a tuple.
     The first element of the tuple is the string k.
