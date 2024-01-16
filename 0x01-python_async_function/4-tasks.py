96391760c969a65ed22b6b5052d7912ab0c7377f
Omar Khaled <o.khaled6789@gmail.com>
2024-01-16 04:55:22 +0200

    fixed the pycodestyling

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,8 +10,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    The code is nearly identical to wait_n except task_wait_random is being called.
-    The funtion spawns task_wait_random (n) times with the specified max_delay.
+    The code is nearly identical to wait_n except task_wait_random is
+    being called. The funtion spawns task_wait_random (n) times with
+    the specified max_delay.
     """
     tasks = []
     for i in range(n):
