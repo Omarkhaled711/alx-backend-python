de2486cdd72fde68dd48d1c33a3bec4937759bef
Omar Khaled <o.khaled6789@gmail.com>
2024-01-29 00:59:16 +0200

    added tasks

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+third task module
+"""
+
+from asyncio import gather
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def async_comprehension() -> float:
+    """
+    a measure_runtime coroutine that will execute async_comprehension
+    four times in parallel using asyncio.gather.
+    """
+    start = time()
+    coroutines = [async_comprehension() for _ in range(4)]
+    await gather(*coroutines)
+    return time() - start
